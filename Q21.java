65d38cf0c65b3bc561702760649ca39545ef65e2
Anshuman <anshuman0042@gmail.com>
2025-01-21 21:25:37 +0530

    Javalab 2 question 1 redone

--- Q21.java
+++ Q21.java
@@ -1,35 +1,40 @@
-// todo : . Write a java program to print "N" prime numbers within the range of X (lower limit) and Y (upper limit). Prompt the values of X, Y, and N from the user. Ensure that the value Y is greater than X and all the numbers are positive whole numbers. For any invalid input, ask for the input again.
-
 import java.util.Scanner;
 class Q21{
-    public static void main(String [] args){
+    public static void main(String[] args){
         Scanner s=new Scanner(System.in);
-        int x,y,N;
-        while(true){
-        System.out.print("Enter the first element: ");
-        x=s.nextInt();
-        System.out.print("Enter the second element: ");
-        y=s.nextInt();
-        System.out.print("Enter the number of primes needed ");
-        N=s.nextInt();
-        int c =0;
+        System.out.println("Enter the number of numbers needed");
+        int N=s.nextInt();
+        int c=0;
+        while(c<N){
+        System.out.println("Enter the lower limit for search");
+        int x = s.nextInt();
+        System.out.println("Enter the Upper limit for search");
+        int y=s.nextInt();
         if(x>y){
-            System.out.println("X>Y not allowed reinput and retry");
-            continue;
+            System.out.println("Invalid input");
+            break;
         }
-        else{
-            while(c<N){
-                for(int i=2;i<y;i++){
-                    if(isPrime(i) && i>x){
- System.out.println("the prime in range is : " + i);
-                    }
-                    
-                }           
-                c=c+1;
-                }
+        int nums=1;
+        for(int i =x;i<y;i++){
+            if(i>1){
+                boolean isPrime=true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j ==0){
+                    isPrime=false;
+                    break;
                 }
             }
-           
-        
+            if(isPrime){
+                
+            System.out.println(i);
+            }
+            }
+            
+            
+        }
+            c=c+1;
+        }
+        System.out.println();
     }
+
 }
