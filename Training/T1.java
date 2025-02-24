630a32443e614eda136ac3d3324bf7c585acc6f4
Anshuman <anshuman0042@gmail.com>
2025-02-24 10:15:49 +0530

    This is the folder that has Java lab contents

--- Training/T1.java
+++ Training/T1.java
@@ -1,28 +1,26 @@
-package utils;
-import java.util.*;
-import java.io.*;
-import java.math.*;
-import java.util.stream.*;
-import java.nio.file.*;
-import java.text.*;
-class T1{ 
-        public static void main(String args[]){
-            Scanner s=new Scanner(System.in);
-            int n=s.nextInt();
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
+import java.util.Scanner;
+class printZero{
+    public int n;
+    public printZero(n){
+            for(int i=0;i<=n;i++){
+                for(int j=0;j<=n;j++){
                     if(i==0|| i==n||j==0||j==n){
-                        System.out.print("*");
+                        System.out.print("* ");
                     }
                     else{
-                        System.out.print(" ");
+                        System.out.print("  ");
 
                     }
-                    System.out.println();
                 }
-
-
+                    System.out.println();
             }
-
+    }
+}
+class T1{
+    public static void main(String arg[]){
+         Scanner s=new Scanner(System.in);
+            int n=s.nextInt();
+        
+            
     }
 }
